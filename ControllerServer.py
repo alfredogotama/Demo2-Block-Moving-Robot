d631ab8caafa0488e4ab1fe56f30b6e2ac396140
alfredogotama <aany@gmu.edu>
2015-04-29 14:56:10 -0400

    Update ControllerServer.py

--- ControllerServer.py
+++ ControllerServer.py
@@ -1,1 +1,22 @@
+import socket
+import serial
+import sys
 
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server_address = ('', 10000)
+sock.bind(server_address)
+
+sock.listen(1)
+
+connection, clientAddress = sock.accept() #Not sure if works
+
+ser = serial.Serial("/dev/ttyACM0", 9600)
+
+while True:
+   connection.sendall("Insert car command here: (a b c d e f g h i j r s ")
+   command = connnection.recv(5) #Not sure
+   ser.write(str(command))
+
+connection.close()
+   
